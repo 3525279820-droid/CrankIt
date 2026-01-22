55341aa03fcb864982219401df74faba1f7cf163
P0PPY_ <3525279820@qq.com>
2026-01-22 22:51:10 +0800

    Main menu Tunnel

--- Source/CrankIt/BgCamera.cpp
+++ Source/CrankIt/BgCamera.cpp
@@ -3,12 +3,28 @@
 
 #include "BgCamera.h"
 
+
+
 // Sets default values
 ABgCamera::ABgCamera()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SetRootComponent(SpringArmComp);
+	
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("BackgroundCamera");
+	CameraComp->SetupAttachment(SpringArmComp);
 
+	LightComponent = CreateDefaultSubobject<UPointLightComponent>("LightComp");
+	LightComponent->SetupAttachment(CameraComp);
+}
+
+void ABgCamera::ResetPosition()
+{
+	FVector CameraLocation = SpringArmComp->GetComponentLocation();
+	CameraLocation.Z = -1300.f;
+	SpringArmComp->SetWorldLocation(CameraLocation);
 }
 
 // Called when the game starts or when spawned
@@ -22,6 +38,11 @@ void ABgCamera::BeginPlay()
 void ABgCamera::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	// FVector CameraLocation = SpringArmComp->GetComponentLocation();
+	// SpringArmComp->AddLocalOffset(FVector(0, 0 , DeltaTime * -500.f));
+	// if(2810.f -  FMath::Abs(CameraLocation.Z)< KINDA_SMALL_NUMBER)
+	// {
+	// 	ResetPosition();
+	// }
 }
 
--- Source/CrankIt/BgCamera.h
+++ Source/CrankIt/BgCamera.h
@@ -3,18 +3,32 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "Components/PointLightComponent.h"
+#include "GameFramework/Pawn.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 #include "BgCamera.generated.h"
 
 UCLASS()
-class CRANKIT_API ABgCamera : public AActor
+class CRANKIT_API ABgCamera : public APawn
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
 	ABgCamera();
+	
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+	
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
 
+	UPROPERTY(VisibleAnywhere)
+	UPointLightComponent* LightComponent;
+
+	void ResetPosition();
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
